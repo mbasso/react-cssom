1f41cf469c6b710d3440d8e8267f26538172860c
mbasso <mbasso96@gmail.com>
2017-01-11 22:34:25 +0100

    Remove mount and unmount

--- src/index.js
+++ src/index.js
@@ -6,22 +6,6 @@ const ReactCSSOM = {
     React.Component = inject(React.Component)();
   },
   inject,
-  mount: (css) => {
-    const head = document.head || document.getElementsByTagName('head')[0];
-    const style = document.createElement('style');
-    style.type = 'text/css';
-    if (style.styleSheet) {
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
-    }
-    head.appendChild(style);
-    return style;
-  },
-  unmount: (style) => {
-    const head = document.head || document.getElementsByTagName('head')[0];
-    head.removeChild(style);
-  },
 };
 
 export default ReactCSSOM;
